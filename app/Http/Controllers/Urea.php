fb229ce22faca447f0f0ef4e14d2690976eee1cd
Sergiorv87 <s87@hotmail.com>
2019-01-10 11:51:41 +0100

    Añadimos el login

--- app/Http/Controllers/Urea.php
+++ app/Http/Controllers/Urea.php
@@ -7,4 +7,26 @@ use Illuminate\Http\Request;
 class Urea extends Controller
 {
     //
+    function login(Request $req){
+        $usr= $req->get('em');
+        $pass= $req->get('pass');
+        
+        $personas = \DB::table('usuario')
+        ->select('nombre', 'password', 'email')
+        ->where('email', '=', $usr)
+        ->where('password', '=', $pass)
+        ->get();
+        
+        if(sizeof($personas)>0)
+        {
+            return view('panelUrea');
+        }
+       else
+       {
+           $datos=[
+                'msg'=>'Usuario y/o contraseña incorrectos'  
+            ];
+        return view('inicio',$datos);
+       }
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('inicio');
 });
+
+Route::post('login','Urea@login');
